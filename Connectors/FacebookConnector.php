ecaaf678b7f2a2f79d37a96687b9594892d463aa
Calin Rada <rada.calin@gmail.com>
2015-03-11 17:49:53 +0100

    Updated facebook connector to be compatible with sdk 4.0

--- Connectors/FacebookConnector.php
+++ Connectors/FacebookConnector.php
@@ -1,157 +1,61 @@
 <?php
 namespace Phalcon\UserPlugin\Connectors;
 
+use Facebook\FacebookSession;
+use Facebook\FacebookRequest;
+use Facebook\GraphUser;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookRedirectLoginHelper;
+
 /**
  * Phalcon\UserPlugin\Connectors\FacebookConnector
- *
- * This is a modified class of the official class in order to use
- * Phalcon's session and cookie management
- *
- * @see https://github.com/facebook/facebook-php-sdk/blob/master/src/facebook.php
  */
-class FacebookConnector extends \BaseFacebook
+class FacebookConnector
 {
     private $di;
 
-    const FBSS_COOKIE_NAME = 'fbss';
+    private $fb_session;
 
-    const FBSS_COOKIE_EXPIRE = 31556926; // 1 year
+    private $helper;
 
-    protected $sharedSessionID;
-
-    protected static $kSupportedKeys = array('state', 'code', 'access_token', 'user_id');
+    private $url;
 
     public function __construct($di)
     {
-        $this->di = $di;
+        $this->di  = $di;
+        $fbConfig  = $di->get('config')->pup->connectors->facebook;
+        $protocol  = strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,strpos( $_SERVER["SERVER_PROTOCOL"],'/'))).'://';
+        $this->url = $protocol.$_SERVER['HTTP_HOST'].'/user/loginWithFacebook';
 
-        $fbConfig = $di->get('config')->pup->connectors->facebook;
-
-        $config['appId'] = $fbConfig->appId;
-        $config['secret'] = $fbConfig->secret;
-
-        parent::__construct($config);
-
-        if (!empty($config['sharedSession'])) {
-            $this->initSharedSession();
-        }
+        FacebookSession::setDefaultApplication($fbConfig->appId, $fbConfig->secret);
     }
 
-    protected function initSharedSession()
+    public function getLoginUrl($scope = [])
     {
-        $o_cookies = $this->di->get('cookies');
-        $cookie_name = $this->getSharedSessionCookieName();
+        $this->helper = new FacebookRedirectLoginHelper($this->url);
 
-        if ($o_cookies->has($cookie_name)) {
-            $data = $this->parseSignedRequest($o_cookies->get($cookie_name)->getValue());
-            if ($data && !empty($data['domain']) &&
-                self::isAllowedDomain($this->getHttpHost(), $data['domain'])) {
-                // good case
-                $this->sharedSessionID = $data['id'];
-
-                return;
-            }
-            // ignoring potentially unreachable data
-        }
-        // evil/corrupt/missing case
-        $base_domain = $this->getBaseDomain();
-        $this->sharedSessionID = md5(uniqid(mt_rand(), true));
-        $cookie_value = $this->makeSignedRequest(
-            array(
-                'domain' => $base_domain,
-                'id' => $this->sharedSessionID,
-            )
-        );
-        $o_cookies->set($cookie_name, $cookie_value);
-
-        if (!headers_sent()) {
-            $expire = time() + self::FBSS_COOKIE_EXPIRE;
-            //setcookie($cookie_name, $cookie_value, $expire, '/', '.'.$base_domain);
-            $o_cookies->set($cookie_name, $cookie_value, $expire, '/', null, '.'.$base_domain);
-        } else {
-            // @codeCoverageIgnoreStart
-            self::errorLog(
-                'Shared session ID cookie could not be set! You must ensure you '.
-                'create the Facebook instance before headers have been sent. This '.
-                'will cause authentication issues after the first request.'
-            );
-            // @codeCoverageIgnoreEnd
-        }
+        return $this->helper->getLoginUrl($scope);
     }
 
-    /**
-     * Provides the implementations of the inherited abstract
-     * methods.  The implementation uses PHP sessions to maintain
-     * a store for authorization codes, user ids, CSRF states, and
-     * access tokens.
-     */
-    protected function setPersistentData($key, $value)
+    public function getUser()
     {
-        if (!in_array($key, self::$kSupportedKeys)) {
-            self::errorLog('Unsupported key passed to setPersistentData.');
-
-            return;
+        try {
+            $this->helper     = new FacebookRedirectLoginHelper($this->url);
+            $this->fb_session = $this->helper->getSessionFromRedirect();
+        } catch(FacebookRequestException $ex) {
+            $this->di->flashSession->error($ex->getMessage());
+        } catch(\Exception $ex) {
+            $this->di->flashSession->error($ex->getMessage());
         }
 
-        $session_var_name = $this->constructSessionVariableName($key);
-        $this->di->get('session')->set($session_var_name, $value);
-    }
+        if ($this->fb_session) {
+            $request  = new FacebookRequest($this->fb_session, 'GET', '/me');
+            $response = $request->execute();
+            $fb_user  = $response->getGraphObject()->asArray();
 
-    protected function getPersistentData($key, $default = false)
-    {
-        if (!in_array($key, self::$kSupportedKeys)) {
-            self::errorLog('Unsupported key passed to getPersistentData.');
-
-            return $default;
+            return $fb_user;
         }
 
-        $session_var_name = $this->constructSessionVariableName($key);
-
-        return $this->di->get('session')->has($session_var_name) ?
-        $this->di->get('session')->get($session_var_name) : $default;
-    }
-
-    protected function clearPersistentData($key)
-    {
-        if (!in_array($key, self::$kSupportedKeys)) {
-            self::errorLog('Unsupported key passed to clearPersistentData.');
-
-            return;
-        }
-
-        $session_var_name = $this->constructSessionVariableName($key);
-        $this->di->get('session')->remove($session_var_name);
-    }
-
-    protected function clearAllPersistentData()
-    {
-        foreach (self::$kSupportedKeys as $key) {
-            $this->clearPersistentData($key);
-        }
-        if ($this->sharedSessionID) {
-            $this->deleteSharedSessionCookie();
-        }
-    }
-
-    protected function deleteSharedSessionCookie()
-    {
-        $cookie_name = $this->getSharedSessionCookieName();
-        $this->di->get('cookies')->delete($cookie_name);
-        $base_domain = $this->getBaseDomain();
-    }
-
-    protected function getSharedSessionCookieName()
-    {
-        return self::FBSS_COOKIE_NAME . '_' . $this->getAppId();
-    }
-
-    protected function constructSessionVariableName($key)
-    {
-        $parts = array('fb', $this->getAppId(), $key);
-        if ($this->sharedSessionID) {
-            array_unshift($parts, $this->sharedSessionID);
-        }
-
-        return implode('_', $parts);
+        return false;
     }
 }
