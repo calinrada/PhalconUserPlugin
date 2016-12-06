b19705991c70ccbbc0bdb3d707100e8bcab21f44
Calin Rada <rada.calin@gmail.com>
2016-12-06 16:03:19 +0100

    Switched to facebook/graph-sdk

--- lib/Migration/Db.php
+++ lib/Migration/Db.php
@@ -6,10 +6,14 @@ class Db extends \Phalcon\Mvc\User\Plugin
 {
     public static function migrate()
     {
-        $db = \Phalcon\DI::getDefault()->get('db');
-        $db->execute('ALTER TABLE `user_email_confirmations` CHANGE  `modified_at`  `updated_at` DATETIME NULL DEFAULT NULL ;');
-        $db->execute('ALTER TABLE `user` DROP COLUMN profile_id ;');
-        $db->execute('ALTER TABLE `user` ADD COLUMN `status` tinyint(2) NOT NULL DEFAULT \'1\'');
-        $db->execute('ALTER TABLE `user` CHANGE `linkedin_id` `linkedin_id` VARCHAR( 64 ) NULL DEFAULT NULL ;');
+        try {
+            $db = \Phalcon\DI::getDefault()->get('db');
+            $db->execute('ALTER TABLE `user_email_confirmations` CHANGE  `modified_at`  `updated_at` DATETIME NULL DEFAULT NULL ;');
+            $db->execute('ALTER TABLE `user` DROP COLUMN profile_id ;');
+            $db->execute('ALTER TABLE `user` ADD COLUMN `status` tinyint(2) NOT NULL DEFAULT \'1\'');
+            $db->execute('ALTER TABLE `user` CHANGE `linkedin_id` `linkedin_id` VARCHAR( 64 ) NULL DEFAULT NULL ;');
+        } catch (\Exception $e) {
+            echo 'Database migration error: '.$e->getMessage();
+        }
     }
 }
