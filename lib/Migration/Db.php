f756b279990fc85e392dc8e628d6b656694b54a0
shovv <marek@shovv.pl>
2023-09-07 20:52:43 +0200

    updated and restored migration db queries

--- lib/Migration/Db.php
+++ lib/Migration/Db.php
@@ -9,12 +9,11 @@ class Db extends \Phalcon\Di\Injectable
         try {
             // @TODO upgrade phalcon db class and methods
 
-            /* $db = \Phalcon\DI::getDefault()->get('db');
+            $db = \Phalcon\Di\Di::getDefault()->get('db');
             $db->execute('ALTER TABLE `user_email_confirmations` CHANGE  `modified_at`  `updated_at` DATETIME NULL DEFAULT NULL ;');
             $db->execute('ALTER TABLE `user` DROP COLUMN profile_id ;');
             $db->execute('ALTER TABLE `user` ADD COLUMN `status` tinyint(2) NOT NULL DEFAULT \'1\'');
-            $db->execute('ALTER TABLE `user` CHANGE `linkedin_id` `linkedin_id` VARCHAR( 64 ) NULL DEFAULT NULL ;'); */
-            echo 'All good...';
+            $db->execute('ALTER TABLE `user` CHANGE `linkedin_id` `linkedin_id` VARCHAR( 64 ) NULL DEFAULT NULL ;'); 
         } catch (\Exception $e) {
             echo 'Database migration error: '.$e->getMessage();
         }
